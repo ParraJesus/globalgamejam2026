544d864d1e85e611623f5eead5c8ed3c089c0668
Edwin Orlando Restrepo Mosquera <edwin415@outlook.com>
2026-01-30 11:39:12 -0500

    Last changes on UI

---
+++ who/Assets/_Project/ScriptableObjects/RoundNarrativeTransition.cs
@@ -0,0 +1,8 @@
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "RoundNarrativeTransition", menuName = "Scriptable Objects/RoundNarrativeTransition")]
+public class RoundNarrativeTransition : ScriptableObject
+{
+    public rounds round;
+    public NarrativeDialogueData narrativeTransition;
+}
--- who/Assets/_Project/Scripts/UI/DialogueUI.cs
+++ who/Assets/_Project/Scripts/UI/DialogueUI.cs
@@ -13,6 +13,7 @@ public class DialogueUI : MonoBehaviour
     public TextMeshProUGUI dialogueText;
     public Image portraitImage;
     public TextMeshProUGUI npcName;
+    [SerializeField] public Sprite PlaceHolder;
 
     [Header("Buttons")]
     public Button questionButton;
@@ -227,8 +228,8 @@ public class DialogueUI : MonoBehaviour
         narrativeLines = data.lines;
         narrativeIndex = 0;
 
-        portraitImage.gameObject.SetActive(false);
-        npcName.gameObject.SetActive(false);
+        portraitImage.sprite = PlaceHolder;
+        npcName.text = "Parra";
         questionButton.gameObject.SetActive(false);
         pressureButton.gameObject.SetActive(false);
         accuseButton.gameObject.SetActive(false);
